09bca46e07d8bfd7ae6801e89fac3c8fe28a08d7
Yusuke Sato <yusuke_s@YusukenoMacBook-ea.local>
2020-04-04 09:20:01 +0900

    Update route for hiding post create feature

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index');
-Route::resource('/articles', 'ArticleController')->except(['index']);
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
